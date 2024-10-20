472e6eb311f376f1431bfdccb56d956f510d7b23
JavohirAliyev <jakhaaliyev@gmail.com>
2024-10-20 16:27:15 +0300

    half done

--- Tasks/Arrays.cs
+++ Tasks/Arrays.cs
@@ -12,17 +12,41 @@ public class Arrays
 {
     public int FindMissingNumber(int[] nums)
     {
-        throw new NotImplementedException();
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i-1] + 1)
+            {
+                return nums[i];
+            }
+        }
+
+        return 0;
     }
 
     public int FindPeakElement(int[] nums)
     {
-        throw new NotImplementedException();
+        for (int i = 1; i < nums.Length - 1; i++)
+        {
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+            {
+                return i;
+            }
+        }
+
+        return 0;
     }
 
     public bool IsArraySorted(int[] array)
     {
-        throw new NotImplementedException();
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < array[i-1])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public int MaxValueInArray(int[] array)
@@ -37,17 +61,36 @@ public class Arrays
 
     public void MergeSortedArrays(int[] nums1, int m, int[] nums2, int n)
     {
-        throw new NotImplementedException();
+        int i = 0;
+        int j = 0;
+        while(i < m){
+            if(nums1[i] > nums2[j]){
+                
+            }
+        }
     }
 
     public int RemoveDuplicates(int[] array)
     {
-        throw new NotImplementedException();
+        int n = array.Length;
+        for(int i = 1; i < array.Length - 1; i++){
+            if(array[i] == array[i - 1]){
+                array[i] = array[i + 1]
+                n--;
+            }
+        }
+
+        return n;
     }
 
     public int[] ReverseArray(int[] array)
     {
-        throw new NotImplementedException();
+        for(int i = 0; i < array.Length / 2; i++)
+        {
+            (array[i], array[array.Length - i - 1]) = (array[array.Length - i - 1], array[i]);
+        }
+
+        return array;
     }
 
     public void RotateArray(int[] array, int k)
@@ -62,6 +105,12 @@ public class Arrays
 
     public int SumOfEvenNumbers(int[]? array)
     {
-        throw new NotImplementedException();
+        int sum = 0;
+        for(int i = 0; i< array.Length; i++){
+            if(array[i] % 2 == 0)
+                sum += array[i];
+        }
+
+        return sum;
     }
 }
